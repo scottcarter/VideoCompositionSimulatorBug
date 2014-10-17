70f963fd2e20ebe9d81792dea7502dcaf205f327
Scott Carter <scarter28m-github@yahoo.com>
2014-10-17 09:24:57 -0400

    Initial Commit

---
+++ VideoCompositionSimulatorBug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoCompositionSimulatorBug
+//
+//  Created by Scott Carter on 10/17/14.
+//  Copyright (c) 2014 Scott Carter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoCompositionSimulatorBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoCompositionSimulatorBug
+//
+//  Created by Scott Carter on 10/17/14.
+//  Copyright (c) 2014 Scott Carter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
